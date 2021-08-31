9a22c2310b931e1a70d3ffca0cd4418af75266cf
ferminrp <frodriguezpenelas@gmail.com>
2021-08-31 12:09:07 -0300

    listado sin imagenes

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Tabla from "./UI/Tabla.svelte";
+
+  let data = [];
+
+  const cedeares = fetch(
+    "https://sheets.googleapis.com/v4/spreadsheets/1NDOyoL3PGNe-rAm-eMHGrLKLASE6j_tUjkJ3lwXTqu0/values/main!A2:D193?key=AIzaSyBhiqVypmyLHYPmqZYtvdSvxEopcLZBdYU"
+  )
+    .then((response) => response.json())
+    .then(info => {
+		for (let i = 0; i < info.values.length; i++) {
+			let row = {}
+			row.symbol = info.values[i][0];
+			row.name = info.values[i][1];
+			row.price = info.values[i][2];
+			row.change = info.values[i][3];
+			data = [...data, row];
+		}
+	});
+</script>
+
+<h1>Listado de Cedeares Disponibles</h1>
+
+{#if data.length > 0}
+<Tabla {data} />
+{/if}
---
+++ src/UI/Tabla.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let data;
+    /*https://gridjs.io/docs/hello-world*/
+    import Grid from "gridjs-svelte";
+
+    let grid
+
+	$: if (grid) {
+		// add plugin
+		grid.updateConfig(
+			{search: {
+    			enabled: true
+			},
+			sort: {
+				enabled: true
+			},
+			fixedHeader: {
+				enabled: true
+			}
+		});
+		
+		// we need to force rendering so the plugin can show up
+		grid.forceRender()
+	}
+</script>
+
+<Grid bind:instance={grid} {data} />
+
+<style global>
+	@import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+  </style>
