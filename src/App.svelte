117e12b56412cd8ae42334503581564bd46065bc
ferminrp <frodriguezpenelas@gmail.com>
2021-09-03 17:05:39 -0300

    share widget

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,24 @@
 <script>
   import Tabla from "./UI/Tabla.svelte";
   import { BarLoader } from "svelte-loading-spinners";
-  import Callout from "./UI/Callout.svelte"
-  import Search from "./UI/Search.svelte"
+  import Callout from "./UI/Callout.svelte";
+  import Search from "./UI/Search.svelte";
+  import Share from "./UI/Share.svelte";
 
   let data = [];
   let columns = ["", "Symbol", "Name", "Price", "Daily"];
   let searchedValue = "";
   let filteredData = [];
-  
+
   $: {
-    if (searchedValue === "" ){
+    if (searchedValue === "") {
       filteredData = data;
     } else {
-      filteredData = data.filter(cedear => cedear.symbol.includes(searchedValue.toUpperCase()) || cedear.name.toUpperCase().includes(searchedValue.toUpperCase()));
+      filteredData = data.filter(
+        (cedear) =>
+          cedear.symbol.includes(searchedValue.toUpperCase()) ||
+          cedear.name.toUpperCase().includes(searchedValue.toUpperCase())
+      );
     }
   }
 
@@ -36,38 +41,53 @@
       urlReader();
     });
 
-    function search(e) {
-      searchedValue = e.detail.searchedValue;
-      if (searchedValue === "") {
-        window.history.pushState({page: "Listado de Cedears"}, "Listado de Cedears",window.location.origin);
-      } else {
-      window.history.pushState({page: "Listado de Cedears"}, "Listado de Cedears",window.location.origin+"/?search="+searchedValue);
-    };
-      window.splitbee.track("Search", {
-        searchedValue: searchedValue
-      });
+  function search(e) {
+    searchedValue = e.detail.searchedValue;
+    if (searchedValue === "") {
+      window.history.pushState(
+        { page: "Listado de Cedears" },
+        "Listado de Cedears",
+        window.location.origin
+      );
+    } else {
+      window.history.pushState(
+        { page: "Listado de Cedears" },
+        "Listado de Cedears",
+        window.location.origin + "/?search=" + searchedValue
+      );
     }
+    window.splitbee.track("Search", {
+      searchedValue: searchedValue,
+    });
+  }
 
-    function urlReader() {
-      // Read search params in url
-      let searchParams = window.location.search;
-      let searchQuery = new URLSearchParams(searchParams);
-      let searchValue = searchQuery.get("search");
-      searchValue !== null ? (searchedValue = searchValue) : (searchedValue = "");
-    }
+  function urlReader() {
+    // Read search params in url
+    let searchParams = window.location.search;
+    let searchQuery = new URLSearchParams(searchParams);
+    let searchValue = searchQuery.get("search");
+    searchValue !== null ? (searchedValue = searchValue) : (searchedValue = "");
+  }
 </script>
 
 <main>
   <h1>Listado de CEDEARs</h1>
 
-  <Callout color="#FDD2C1">Bienvenido! Aca vas a poder analizar todos los CEDEARs que actualmente cotizan en el mercado. Pronto vas a poder guardar tus favoritos para seguir tus inversiones.</Callout>
-  <Search on:search="{search}" {searchedValue}></Search>
+  <Callout color="#FDD2C1"
+    >Bienvenido! Aca vas a poder analizar todos los CEDEARs que actualmente
+    cotizan en el mercado. Pronto vas a poder guardar tus favoritos para seguir
+    tus inversiones.</Callout
+  >
+  <Search on:search={search} {searchedValue} />
   {#if data.length > 0}
-    <Tabla data="{filteredData}" {columns} />
+    <Tabla data={filteredData} {columns} />
   {:else}
     <div class="loader">
       <BarLoader />
     </div>
+  {/if}
+  {#if searchedValue.length > 0}
+    <Share />
   {/if}
 </main>
 
@@ -79,8 +99,8 @@
   }
 
   h1 {
-	font-family: "Nunito", sans-serif;
-	font-weight: 800;
+    font-family: "Nunito", sans-serif;
+    font-weight: 800;
   }
 
   .loader {
