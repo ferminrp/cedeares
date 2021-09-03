fa132d4a2e56043ac44d6fa5698f8247e158aa9a
ferminrp <frodriguezpenelas@gmail.com>
2021-09-03 12:25:06 -0300

    search mvp

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,20 @@
   import Tabla from "./UI/Tabla.svelte";
   import { BarLoader } from "svelte-loading-spinners";
   import Callout from "./UI/Callout.svelte"
+  import Search from "./UI/Search.svelte"
 
   let data = [];
   let columns = ["", "Symbol", "Name", "Price", "Daily"];
+  let searchedValue = "";
+  let filteredData = [];
+  
+  $: {
+    if (searchedValue === "" ){
+      filteredData = data;
+    } else {
+      filteredData = data.filter(cedear => cedear.symbol.includes(searchedValue.toUpperCase()) || cedear.name.toUpperCase().includes(searchedValue.toUpperCase()));
+    }
+  }
 
   const cedeares = fetch(
     "https://sheets.googleapis.com/v4/spreadsheets/1NDOyoL3PGNe-rAm-eMHGrLKLASE6j_tUjkJ3lwXTqu0/values/main!A2:E193?key=AIzaSyBhiqVypmyLHYPmqZYtvdSvxEopcLZBdYU"
@@ -23,15 +34,19 @@
         data = [...data, row];
       }
     });
+
+    function search(e) {
+      searchedValue = e.detail.searchedValue;
+    }
 </script>
 
 <main>
   <h1>Listado de CEDEARs</h1>
 
- <Callout color="#FDD2C1">Bienvenido! Aca vas a poder analizar todos los CEDEARs que actualmente cotizan en el mercado. Pronto vas a poder guardar tus favoritos para seguir tus inversiones.</Callout>
-
+  <Callout color="#FDD2C1">Bienvenido! Aca vas a poder analizar todos los CEDEARs que actualmente cotizan en el mercado. Pronto vas a poder guardar tus favoritos para seguir tus inversiones.</Callout>
+  <Search on:search="{search}"></Search>
   {#if data.length > 0}
-    <Tabla {data} {columns} />
+    <Tabla data="{filteredData}" {columns} />
   {:else}
     <div class="loader">
       <BarLoader />
---
+++ src/UI/Search.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+
+    let searchedValue = '';
+
+    function search() {
+        dispatch('search', {
+            searchedValue
+        })
+    }
+</script>
+
+<input on:keyup="{search}" bind:value="{searchedValue}" type="text" placeholder="AAPL">
+
+<style>
+    input {
+        border-radius: 3px;
+    }
+
+    input:focus-visible {
+        outline: none;
+        border: 2px solid #FF3E00;
+    }
+</style>
